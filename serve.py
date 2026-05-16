d847117618255d70a71e0ebe8ee4fc3157f2f988
Chris Butterworth <cbutterworth86@gmail.com>
2026-05-17 00:42:42 +1000

    Remove local helper scripts from repo
    serve.py and build_minutes_docx.py were local dev/build helpers that
    don't belong in the public site repo.

--- serve.py
+++
@@ -1,9 +0,0 @@
-import os, http.server, socketserver
-
-PORT = int(os.environ.get('PORT', 8080))
-Handler = http.server.SimpleHTTPRequestHandler
-Handler.extensions_map['.html'] = 'text/html'
-
-with socketserver.TCPServer(('', PORT), Handler) as httpd:
-    print(f'Serving on port {PORT}')
-    httpd.serve_forever()
